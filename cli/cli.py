3dd9028fa73c3b263e4977dfc8a1c75095d24669
ornit <ornithopterthethopter@gmail.com>
2024-05-09 20:40:40 +0200

    updated cli.py to handle no args provided

--- cli/cli.py
+++ cli/cli.py
@@ -1,15 +1,22 @@
 import sys
 
+def help_menu():
+  with open("cli/helpmenu.txt", "r") as help_menu:
+    print(help_menu.read())
+  sys.exit()
+
 def handle_args() -> str:
   """
   handles command line arguments, shows help menu, starts repl
   """
+
+  if len(sys.argv) < 2:
+    help_menu()
+
   main_arg = sys.argv[1] # <arg>
   
   if main_arg == "help":
-    with open("cli/helpmenu.txt", "r") as help_menu:
-      print(help_menu.read())
-    sys.exit()
+    help_menu()
 
   if main_arg == "repl":
     return "cli/repl.prettyplease" # a script that simply starts the repl
